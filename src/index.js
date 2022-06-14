6684c834e5420c36011c39c5ed3a194d445515e6
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-14 17:14:12 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log(event);
+    const input = document.querySelector('input#searchByID');
+    console.log(input.value);
+    //specify the url with user input
+    try{
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        // get dom elements
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+      //write the data to the page
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+}catch(error){
+    document.querySelector('section#movieDetails h4').innerText = 'Movie not found';
+    document.querySelector('section#movieDetails p').innerText = 'Please try again';
 }
+});
+}
+
 
 document.addEventListener('DOMContentLoaded', init);
